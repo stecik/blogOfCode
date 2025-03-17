81c08290ae22d9f5aa306309de8f7da2d7981b92
Marek Štec <mar.stec@seznam.cz>
2025-03-17 10:31:34 +0100

    user administration fixed, auto tag creation added

--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,8 +1,27 @@
 from rest_framework import serializers
-from .models import Article
+from django.contrib.auth import get_user_model
+from .models import Article, Tag, Category
+
+CustomUser = get_user_model()
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ["name"]
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+
+    tags = serializers.ListField(child=serializers.CharField(), write_only=True)
+    tags_display = serializers.SerializerMethodField()
+    authors = serializers.ListField(child=serializers.IntegerField(), write_only=True)
+    authors_display = serializers.SerializerMethodField()
+    categories = serializers.ListField(
+        child=serializers.IntegerField(), write_only=True
+    )
+    categories_display = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
@@ -11,8 +30,35 @@ class ArticleSerializer(serializers.ModelSerializer):
             "title",
             "content",
             "authors",
+            "authors_display",
             "created_at",
             "updated_at",
             "tags",
+            "tags_display",
             "categories",
+            "categories_display",
         ]
+
+    def create(self, validated_data):
+        tags = validated_data.pop("tags", [])
+        authors = validated_data.pop("authors", [])
+        categories = validated_data.pop("categories", [])
+
+        article = Article.objects.create(**validated_data)
+        article.authors.set(CustomUser.objects.filter(id__in=authors))
+        article.categories.set(Category.objects.filter(id__in=categories))
+
+        for tag in tags:
+            tag_obj, _ = Tag.objects.get_or_create(name=tag)
+            article.tags.add(tag_obj)
+
+        return article
+
+    def get_tags_display(self, obj):
+        return [tag.name for tag in obj.tags.all()]
+
+    def get_authors_display(self, obj):
+        return [author.username for author in obj.authors.all()]
+
+    def get_categories_display(self, obj):
+        return [category.name for category in obj.categories.all()]
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-admin.register(CustomUser)
+admin.site.register(CustomUser, UserAdmin)
